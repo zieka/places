2c25d30718c3585ea49905f036355f9f5938a8fc
Kyle Scully <scullykns@gmail.com>
2016-04-16 20:09:15 -0700

    rspec spec/collection_spec.rb -e rq02

--- app/models/place.rb
+++ app/models/place.rb
@@ -8,4 +8,15 @@ class Place
   def self.collection
     self.mongo_client['places']
   end
+
+  def self.load_all(file)
+    # accept a parameter of type IO with a JSON string of data
+    # read the data from that input parameter (Note: this is similar handling an uploaded file within Rails)
+    # parse the JSON string into an array of Ruby hash objects representing places (Hint: JSON.parse)
+    # insert the array of hash objects into the places collection (Hint: insert_many)
+    files = JSON.parse(file.read)
+    collection.insert_many(files)    
+  end
+
+
 end
