6e75b8a2297af8ad4bd15ae6655aa71293949d4b
Kyle Scully <scullykns@gmail.com>
2016-04-16 20:58:31 -0700

    rspec spec/collection_spec.rb -e rq05

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,24 @@
 class Place
   include Mongoid::Document
+  # a read/write (String) attribute called id
+  # a read/write (String) attribute called formatted_address
+  # a read/write (Point) attribute called location
+  # a read/write (collection of AddressComponents) attribute called address_components
+  attr_accessor :id, :formatted_address, :location, :address_components
+
+
+  # an initialize method to Place that can set the attributes from a hash with
+  # keys _id, address_components, formatted_address, and geometry.geolocation.
+  # (Hint: use .to_s to convert a BSON::ObjectId to a String and BSON::ObjectId.from_string(s) to convert it back again.)
+  def initialize(params={})
+    @id = params[:_id].to_s
+    @formatted_address = params[:formatted_address]
+    @location = Point.new(params[:geometry][:geolocation])
+    @address_components = params[:address_components]
+      .map{ |a| AddressComponent.new(a)} unless params[:address_components].nil?
+  end
+
+
 
   def self.mongo_client
     db = Mongo::Client.new('mongodb://localhost:27017')
@@ -15,8 +34,7 @@ class Place
     # parse the JSON string into an array of Ruby hash objects representing places (Hint: JSON.parse)
     # insert the array of hash objects into the places collection (Hint: insert_many)
     files = JSON.parse(file.read)
-    collection.insert_many(files)    
+    collection.insert_many(files)
   end
-
 
 end
